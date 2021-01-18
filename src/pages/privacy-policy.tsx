a591c5b53558c78ef72716e64870e3dacf5cb6d1
Colin Tindle <colin.tindle@fanduel.com>
2021-01-18 17:08:37 +0000

    add privacy email

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -6,11 +6,25 @@ const PrivacyPolicy: React.FC = () => (
     <div className="m-4 h-full">
         <Card>
             <p>
-                We do not collect or share any personal information. If you
-                choose to log in, we will store a cookie in your browser that
-                identifies you. We store a record of your Discord userid,
+                We do not collect any personal information on this website. If
+                you choose to log in, we will store a cookie in your browser
+                that identifies you. We store a record of your Discord userid,
                 username and avatar. All of this information is purged when you
                 log off.
+            </p>
+            <p>
+                As per the crew&apos;s rules (agreed to by accepting an
+                invitation to the crew on Social Club), we may show usernames of
+                current and past members along with pictures in order to give
+                credit.
+            </p>
+            <p className="text-xs">
+                Privacy contact address: <span>pri</span>
+                <span>vac</span>
+                <span>y@g</span>
+                <span>pad</span>
+                <span>cre</span>
+                <span>w.com</span>
             </p>
         </Card>
         <div className="h-full flex items-center justify-center">
