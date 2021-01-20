45ebd7c3595a2d972956771c8ad4361ed05c380f
Colin Tindle <colin.tindle@fanduel.com>
2021-01-20 12:12:05 +0000

    change logo to RouteLink update 404 page

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,18 +1,16 @@
+import { RouteLink } from "components/primitives"
 import { env } from "helpers"
-import Link from "next/link"
 
 const Logo: React.FC = () => (
-    <Link href="/">
-        <a>
-            <img
-                src={`${env.ASSETS}/logo.png`}
-                alt="GPAD Logo"
-                className="my-2"
-                width={40}
-                height={40}
-            />
-        </a>
-    </Link>
+    <RouteLink to="/" title="Home">
+        <img
+            src={`${env.ASSETS}/logo.png`}
+            alt="GPAD Logo"
+            className="my-2"
+            width={40}
+            height={40}
+        />
+    </RouteLink>
 )
 
 export default Logo
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,14 @@
-const NotFound: React.FC = () => <div className="m-4">404</div>
+import { RouteLink } from "components/primitives"
+
+const NotFound: React.FC = () => (
+    <div className="mt-16 h-full flex flex-col items-center">
+        <p>You&apos;re lost?</p>
+        <p>
+            <RouteLink to="/" title="Home" bold>
+                Return Home
+            </RouteLink>
+        </p>
+    </div>
+)
 
 export default NotFound
