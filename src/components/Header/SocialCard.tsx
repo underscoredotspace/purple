1a46775dbd02f875e99543cc29f81afa51a3c27e
Colin <underscoredotspace@users.noreply.github.com>
2022-06-13 09:06:19 +0000

    update social card

--- src/components/Header/SocialCard.tsx
+++ src/components/Header/SocialCard.tsx
@@ -1,22 +1,22 @@
-import Head from "next/head"
+import Head from "next/head";
 
 const SocialCard: React.FC = () => (
-    <Head>
-        <meta property="og:title" content="Gettin' Payd All Day (GPAD)" />
-        <meta
-            property="og:description"
-            content="An active and supportive dual-platform (PlayStation 4 and Xbox One) Grand Theft Auto Online crew and gaming community"
-        />
-        <meta property="og:url" content="https://gpadcrew.com" />
-        <meta
-            name="og:image"
-            content="https://gpadcrew.com/assets/social-card.jpg"
-        />
-        <meta name="theme-color" content="#0f2499" />
+  <Head>
+    <meta property="og:title" content="Gettin' Payd All Day (GPAD)" />
+    <meta
+      property="og:description"
+      content="An active and supportive multi-platform (PlayStation, Xbox and PC) Grand Theft Auto Online crew and gaming community"
+    />
+    <meta property="og:url" content="https://gpadcrew.com" />
+    <meta
+      name="og:image"
+      content="https://gpadcrew.com/assets/social-card.jpg"
+    />
+    <meta name="theme-color" content="#0f2499" />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image:alt" content="GPAD GTA Crew" />
-    </Head>
-)
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:image:alt" content="GPAD GTA Crew" />
+  </Head>
+);
 
-export default SocialCard
+export default SocialCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,74 +1,80 @@
-import { faPlaystation, faXbox } from "@fortawesome/free-brands-svg-icons"
-import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { CrewStats, DiscordInvite, YouTube } from "components"
-import { RouteLink, SectionTitle } from "components/primitives"
+import {
+  faPlaystation,
+  faWindows,
+  faXbox,
+} from "@fortawesome/free-brands-svg-icons";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { CrewStats, DiscordInvite, YouTube } from "components";
+import { RouteLink, SectionTitle } from "components/primitives";
 
 const Home: React.FC = () => (
-    <>
-        <p>
-            We are an active and supportive dual-platform (PlayStation{" "}
-            <FontAwesomeIcon
-                icon={faPlaystation}
-                className="text-blue-200"
-                size="xs"
-            />{" "}
-            and Xbox{" "}
-            <FontAwesomeIcon icon={faXbox} className="text-green-200" size="xs" />)
-            gaming community.
-        </p>
-        <p>
-            Our main games are{" "}
-            <RouteLink to="/gta-online" title="Grand Theft Auto" bold>
-                Grand Theft Auto
-            </RouteLink>
-            ,{" "}
-            <RouteLink to="/red-dead" title="Red Dead Redemption" bold>
-                Red Dead Redemption
-            </RouteLink>{" "}
-            and <strong>Call of Duty</strong>. Our community continues to grow
-            in a number of{" "}
-            <RouteLink to="/other-games" title="Other Games">
-                other
-            </RouteLink>{" "}
-            online multiplayer games.
-        </p>
-        <p>
-            We have an active discord community where members can game together
-            but also share experiences outside of gaming.
-        </p>
+  <>
+    <p>
+      We are an active and supportive multi-platform (PlayStation{" "}
+      <FontAwesomeIcon
+        icon={faPlaystation}
+        className="text-blue-200"
+        size="xs"
+      />{" "}
+      , Xbox{" "}
+      <FontAwesomeIcon icon={faXbox} className="text-green-200" size="xs" /> and
+      PC{" "}
+      <FontAwesomeIcon icon={faWindows} className="text-yellow-200" size="xs" />{" "}
+      ) gaming community.
+    </p>
+    <p>
+      Our main games are{" "}
+      <RouteLink to="/gta-online" title="Grand Theft Auto" bold>
+        Grand Theft Auto
+      </RouteLink>
+      ,{" "}
+      <RouteLink to="/red-dead" title="Red Dead Redemption" bold>
+        Red Dead Redemption
+      </RouteLink>{" "}
+      and <strong>Call of Duty</strong>. Our community continues to grow in a
+      number of{" "}
+      <RouteLink to="/other-games" title="Other Games">
+        other
+      </RouteLink>{" "}
+      online multiplayer games.
+    </p>
+    <p>
+      We have an active discord community where members can game together but
+      also share experiences outside of gaming.
+    </p>
 
-        <YouTube videoId="ulKZbFzPZyw" title="Welcome to GPAD" />
+    <YouTube videoId="ulKZbFzPZyw" title="Welcome to GPAD" />
 
-        <p>
-            Have a look at{" "}
-            <RouteLink to="/about-the-crew" title="About the Crew">
-                about the crew
-            </RouteLink>{" "}
-            to learn more about us. Our{" "}
-            <RouteLink to="/crew-events" title="Our Events">
-                events
-            </RouteLink>{" "}
-            page gives you an insight into what our dedicated team of event
-            coordinators do for the crew.
-        </p>
+    <p>
+      Have a look at{" "}
+      <RouteLink to="/about-the-crew" title="About the Crew">
+        about the crew
+      </RouteLink>{" "}
+      to learn more about us. Our{" "}
+      <RouteLink to="/crew-events" title="Our Events">
+        events
+      </RouteLink>{" "}
+      page gives you an insight into what our dedicated team of event
+      coordinators do for the crew.
+    </p>
 
-        <CrewStats />
+    <CrewStats />
 
-        <SectionTitle>Come to our house and get payd!</SectionTitle>
-        <p>
-            Join our{" "}
-            <DiscordInvite tag="landing">
-                Discord Server
-                <FontAwesomeIcon
-                    icon={faExternalLinkAlt}
-                    className="text-pink-400 ml-1"
-                    size="xs"
-                />
-            </DiscordInvite>{" "}
-            and our Admissions team will help you join.
-        </p>
-    </>
-)
+    <SectionTitle>Come to our house and get payd!</SectionTitle>
+    <p>
+      Join our{" "}
+      <DiscordInvite tag="landing">
+        Discord Server
+        <FontAwesomeIcon
+          icon={faExternalLinkAlt}
+          className="text-pink-400 ml-1"
+          size="xs"
+        />
+      </DiscordInvite>{" "}
+      and our Admissions team will help you join.
+    </p>
+  </>
+);
 
-export default Home
+export default Home;
