a4b01ee3d66b0b46c3138be40fc7590a26eb98b2
Colin <underscoredotspace@users.noreply.github.com>
2022-06-13 08:58:25 +0000

    use webp for logo to make it a bunch smaller

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,15 +1,15 @@
-import { RouteLink } from "components/primitives"
-import { env } from "helpers"
+import { RouteLink } from "components/primitives";
+import { env } from "helpers";
 
 const Logo: React.FC = () => (
-    <RouteLink to="/" title="Home" className="flex-shrink-0">
-        <img
-            src={`${env.ASSETS}/logo.png`}
-            alt="GPAD Logo"
-            width={40}
-            height={40}
-        />
-    </RouteLink>
-)
+  <RouteLink to="/" title="Home" className="flex-shrink-0">
+    <img
+      src={`${env.ASSETS}/logo.webp`}
+      alt="GPAD Logo"
+      width={40}
+      height={40}
+    />
+  </RouteLink>
+);
 
-export default Logo
+export default Logo;
