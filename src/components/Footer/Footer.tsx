d11fb494d008083f5319cea213a3b111b0e5fab1
Colin <underscoredotspace@users.noreply.github.com>
2022-06-24 15:46:15 +0000

    remove fake error

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -56,15 +56,6 @@ export const Footer: React.FC = () => (
         icon={faInstagram}
         text="Instagram"
       />
-      <button
-        hidden
-        type="button"
-        onClick={() => {
-          throw new Error("Sentry Frontend Error");
-        }}
-      >
-        Throw error
-      </button>
     </div>
   </footer>
 );
