5aaee14998e4bec69e702b90bcd381596cd1f0b6
Colin <underscoredotspace@users.noreply.github.com>
2022-06-24 08:49:12 +0000

    remove old settings

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -8,6 +8,4 @@ Sentry.init({
   dsn:
     "https://169bc0e6f1194b0f9ce597bcab686ebb@o1138904.ingest.sentry.io/6193708",
   tracesSampleRate: 1.0,
-  release: process.env.VERSION || process.env.NEXT_PUBLIC_VERSION,
-  environment: process.env.BRANCH === "main" ? "production" : "staging",
 });
