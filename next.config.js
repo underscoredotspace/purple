65086d7f2fcea7a0ff3a1af78bab3b540238eda0
Colin <underscoredotspace@users.noreply.github.com>
2022-06-24 09:23:09 +0000

    🚮

--- next.config.js
+++ next.config.js
@@ -15,24 +15,10 @@ const sentryWebpackPluginOptions = {
   // recommended:
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
-  environment:
-    process.env.NODE_ENV === "development"
-      ? "development"
-      : process.env.CONTEXT === "production"
-      ? "production"
-      : "staging",
   silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
-
-console.log(
-  JSON.stringify({
-    sentryWebpackPluginOptions,
-    env: process.env.NODE_ENV,
-    context: process.env.CONTEXT,
-  })
-);
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
