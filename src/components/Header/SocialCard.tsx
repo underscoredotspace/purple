da7cf44ef68657245affb84e0a7c7dd7e80430b9
underscoredotspace <underscoredotspace@users.noreply.github.com>
2024-01-25 17:15:15 +0000

    new social card

--- src/components/Header/SocialCard.tsx
+++ src/components/Header/SocialCard.tsx
@@ -6,7 +6,7 @@ const SocialCard: React.FC = () => (
       content="An active and supportive PlayStation and Xbox Grand Theft Auto Online crew and gaming community"
     />
     <meta property="og:url" content="https://gpadcrew.com" />
-    <meta name="og:image" content="https://i.gpadcrew.com/social-card.jpg" />
+    <meta name="og:image" content="https://i.gpadcrew.com/gta-style.png" />
     <meta name="theme-color" content="#0f2499" />
 
     <meta name="twitter:card" content="summary_large_image" />
